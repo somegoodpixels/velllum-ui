032e70bad9089d6c27d03bcda44732903dd69529
Josh Smith <josh@quartzstudio.com>
2017-04-02 11:13:26 -0400

    albums now set in vmin units"

--- ui/js/helpers/album.js
+++ ui/js/helpers/album.js
@@ -4,14 +4,14 @@
 
 $('.album').each(function(){
 	var images = $(this).find('.album-image > img');
-	var size = $(this).hasClass('album-small') ? 100 : 200;
+	var size = $(this).hasClass('album-small') ? 20 : 30;
 	images.each(function(){
 		var item = $(this).closest('.album-image');
 		var width = $(this).attr('width');
 		var height = $(this).attr('height');
 		var grow = width / height;
 		var shrink = 1;
-		var basis = (grow*size)+'px'
+		var basis = (grow*size)+'vmin'
 		item.css('flex',grow +' '+ shrink +' '+ basis);
 	});
 	$(this).addClass('album-ready');
