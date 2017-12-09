30e20dc8e1575b5c6bffaa35952852f9228b4df7
Joshua Smith <josh@somegoodpixels.com>
2017-12-09 00:17:50 -0500

    updated ui to enable simplified sites

--- _includes/scripts/helpers.js
+++ _includes/scripts/helpers.js
@@ -1,20 +1,18 @@
-
+// ------------------------------------ READY ------------------------------------
 $(document).ready(function(){ if ( typeof window.velllum_ready === 'function' ) { window.velllum_ready(); } });
 
 // ------------------------------------ COOKIES ------------------------------------
-
 function set_cookie(name,value,days) { var d = new Date(); d.setTime(d.getTime() + ((days?days:60)*24*60*60*1000)); var expires = "expires="+ d.toUTCString(); document.cookie = name + "=" + value + ";" + expires + ";path=/"; }
 function get_cookie(cname) { var name = cname + "="; var ca = document.cookie.split(';'); for(var i = 0; i <ca.length; i++) { var c = ca[i]; while (c.charAt(0)==' ') { c = c.substring(1); } if (c.indexOf(name) == 0) { return c.substring(name.length,c.length); } } return ""; }
 function get_params() { if ( window.location.href.indexOf('?') == -1 ) { return []; } var vars = [], hash; var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&'); for(var i = 0; i < hashes.length; i++) { hash = hashes[i].split('='); vars[hash[0]] = hash[1]; } return vars; }
 function get_param(name) { var params = get_params(); return ( params[name] ) ? params[name] : false; }
 
 // ------------------------------------ CLASSES ------------------------------------
-
-// adds specialty classes to body
+function is_touch(){ return ( 'ontouchstart' in window ); }
 $(document).on('ready',function(){ $('html').addClass('ready'); });
 $(window).on('load',function(){ $('html').addClass('loaded'); });
-if ( 'ontouchstart' in window ) { $('html').addClass('touch'); } else { $('html').addClass('click'); }
-function is_touch(){ return ( 'ontouchstart' in window ); }
+$('html').toggleClass('click',!is_touch());
+$('html').toggleClass('touch',is_touch());
 
 // ------------------------------------ TABS ------------------------------------
 $(document).ready(function(){
@@ -27,65 +25,43 @@ $(document).ready(function(){
 });
 
 // ------------------------------------ SHOWHIDE ------------------------------------
-
-// alternate collapse toggle
 $(document).on('click','[data-toggle="slide"]',function(){ var targets = $($(this).attr('data-target')); targets.each(function(){ if ( $(this).is(':visible') ) { $(this).hide(); } else { $(this).removeClass('hidden').removeAttr('hidden').show(); } }); return false; });
 
 // ------------------------------------ ANCHORS ------------------------------------
-
-// scroll to page section
 $(document).on('click','a[href^="#"]:not("[data-toggle]")',function(e){ var target = $($(this).attr('href')); if (target.length === 0) { return; } e.preventDefault(); var timer = setTimeout(function(){ $('html,body').animate({scrollTop:(target.offset().top)},750,'swing'); },1); });
 
 // ------------------------------------ SCROLLING ------------------------------------
-
 var prevscroll = 0;
-function scroll_classes(){
-	var nextscroll = $(document).scrollTop();
-	if ( nextscroll > prevscroll ) { $('html').addClass('page-scrolling page-scrolling-down').removeClass('page-scrolling-up'); }
-	if ( nextscroll < prevscroll ) { $('html').addClass('page-scrolling page-scrolling-up').removeClass('page-scrolling-down'); }
-	if ( nextscroll == 0 ) { $('html').removeClass('page-scrolling page-scrolling-up page-scrolling-down'); }
+function updateScroll(){
+	var nextscroll = $(document).scrollTop()
+	var winmiddle = nextscroll + $(window).height()/2;
+	$('html').toggleClass('page-scrolling',(nextscroll!=0));
+	$('html').toggleClass('page-scrolling-down',(nextscroll!=0&&nextscroll>prevscroll));
+	$('html').toggleClass('page-scrolling-up',(nextscroll!=0&&nextscroll<prevscroll));
+	$('.layer').each(function(){
+		var top = $(this).offset().top,
+			bottom = top + $(this).outerHeight(),
+			middle = ( top + bottom ) / 2,
+			offset = winmiddle - middle,
+			layer = $(this).data('layer') ? Number($(this).data('layer')) : 0;
+		$(this).css('top',(offset*layer*.1));
+	});
 	prevscroll = nextscroll;
 }
-
-$(window).scroll(function(){ scroll_classes(); });
-$(window).resize(function(){ scroll_classes(); });
-$(document).ready(function(){ scroll_classes(); });
+$(window).scroll(updateScroll);
+$(document).ready(updateScroll);
+$(document).resize(updateScroll);
 
 // ------------------------------------ FOCUS SET ------------------------------------
-
 $(document).on('click','[data-focus]',function(){ var el = $(this); var timer = setTimeout(function(){ $(el.attr('data-focus')).find('input:visible:first').focus(); },1); });
 
-// ------------------------------------ SCROLL INTO VIEW ------------------------------------
-
-function updateScrollFocus(){
-	var wintop = $(document).scrollTop(), winheight = $(window).height(), winmiddle = wintop + winheight/2, winbottom = wintop + winheight;
-	$('[data-scrollfocus]').each(function(){
-		var top = $(this).offset().top, bottom = top + $(this).outerHeight(), middle = ( top + bottom ) / 2;
-		if ( middle < winbottom && bottom > wintop ) {
-			$(this).addClass('scrollfocus'); $(this).removeClass('not-scrollfocus');
-		} else {
-			$(this).addClass('not-scrollfocus'); $(this).removeClass('scrollfocus');
-		}
-	})
-}
-$(window).scroll(function(){ updateScrollFocus(); });
-$(document).ready(function(){ updateScrollFocus(); });
-
 // ------------------------------------ BACKGROUNDS ------------------------------------
-
-// background images
 $(document).ready(function(){ $('[data-bg]').each(function(){ var el = $(this), bg = $(this).data('bg'); if ( bg ) { var timer = setTimeout(function(){ el.css('background-image','url('+bg+')'); },1); } }); });
 
 // ------------------------------------ VIDEO WRAPPER ------------------------------------
-
-// video iframes
 $(document).ready(function(){ $('iframe[src*=vimeo], iframe[src*=youtube]').each(function(){ if ( !$(this).closest('.video') ) { $(this).wrap('<div class="video" />'); } }); });
 
-
-//
-// File Input Buttons
-// --------------------------------------------------
-
+// ------------------------------------ FILES ------------------------------------
 $(document).on('change','input[type="file"]',function(){
 	var btn = $(this).closest('.btn');
 	var files = this.files;
@@ -98,28 +74,18 @@ $(document).on('change','input[type="file"]',function(){
 	}
 });
 
-//
-// Set Focus on Ready
-// --------------------------------------------------
-$(document).ready(function(){
-	var focus = $('.ready-focus').find('input, select, textarea').filter(':visible').filter(':first');
-	if ( focus ) { focus.focus(); }
-});
-
-//
-// Albums
-// --------------------------------------------------
+// ------------------------------------ ALBUM ------------------------------------
 $(document).ready(function(){
 	$('.album').each(function(){
-		var images = $(this).find('.album-image > img');
-		var size = $(this).hasClass('album-small') ? 15 : 25;
+		var images = $(this).find('.album-image > img'),
+			size = $(this).hasClass('album-small') ? 15 : 25;
 		images.each(function(){
-			var item = $(this).closest('.album-image');
-			var width = $(this).attr('width');
-			var height = $(this).attr('height');
-			var grow = width / height;
-			var shrink = 1;
-			var basis = (grow*size)+'vmin'
+			var item = $(this).closest('.album-image'),
+			width = $(this).attr('width'),
+			height = $(this).attr('height'),
+			grow = width / height,
+			shrink = 1,
+			basis = (grow*size)+'vmin'
 			item.css('flex',grow +' '+ shrink +' '+ basis);
 		});
 		$(this).addClass('album-ready');
--- ui/js/scripts.js
+++ ui/js/scripts.js
@@ -6,18 +6,18 @@ layout: raw
 {% include vendors/jquery/jquery-3.2.1.min.js %}
 {% include scripts/helpers.js %}
 
-{% include vendors/jqueryui/jquery-ui-1.12.1/jquery-ui.min.js %}
-{% include scripts/jqueryui.js %}
+{% if site.bootstrap %}
 
-{% include vendors/bootstrap/js/bootstrap.min.js %}
-{% include scripts/bootstrap.js %}
+	{% include vendors/jqueryui/jquery-ui-1.12.1/jquery-ui.min.js %}
+	{% include scripts/jqueryui.js %}
 
-{% include vendors/bootstrap-validator/dist/validator.min.js %}
-{% include scripts/validator.js %}
+	{% include vendors/bootstrap/js/bootstrap.min.js %}
+	{% include scripts/bootstrap.js %}
 
-{% include vendors/bootstrap-suggest/js/bootstrap-suggest.js %}
-{% include scripts/suggest.js %}
+	{% include vendors/bootstrap-validator/dist/validator.min.js %}
+	{% include scripts/validator.js %}
 
-{% include vendors/tiltjs/src/tilt.jquery.js %}
+	{% include vendors/bootstrap-suggest/js/bootstrap-suggest.js %}
+	{% include scripts/suggest.js %}
 
-{% include scripts/parallax.js %}
+{% endif %}
