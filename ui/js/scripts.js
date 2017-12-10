b5472fda8d6c7c03c0b742871112f371ff94eaec
Joshua Smith <josh@somegoodpixels.com>
2017-12-10 14:01:41 -0500

    added simplified styling variant

--- ui/js/scripts.js
+++ ui/js/scripts.js
@@ -6,18 +6,16 @@ layout: raw
 {% include vendors/jquery/jquery-3.2.1.min.js %}
 {% include scripts/helpers.js %}
 
-{% if site.bootstrap %}
-
+{% if site.jqueryui %}
 	{% include vendors/jqueryui/jquery-ui-1.12.1/jquery-ui.min.js %}
 	{% include scripts/jqueryui.js %}
+{% endif %}
 
+{% if site.bootstrap %}
 	{% include vendors/bootstrap/js/bootstrap.min.js %}
 	{% include scripts/bootstrap.js %}
-
 	{% include vendors/bootstrap-validator/dist/validator.min.js %}
 	{% include scripts/validator.js %}
-
 	{% include vendors/bootstrap-suggest/js/bootstrap-suggest.js %}
 	{% include scripts/suggest.js %}
-
 {% endif %}
